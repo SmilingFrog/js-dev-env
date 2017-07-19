f90d930f58907c1bb6956c15b9ac78f632255be6
Sergii Savin <nissserge@gmail.com>
2017-07-19 19:25:05 +0200

    configure mocha

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,8 @@
+// this file is not transpiled. So, must use ES5 and CommonJS
+
+//register babel to transpile before our tests run
+require('babel-register')();
+
+//Disable Webpack features that Mocka does not understand
+//require.extentions['.css'] = function(){};
+
